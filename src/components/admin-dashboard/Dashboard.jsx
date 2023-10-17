c6ad3ba5f53f0b81edabf5227edb4611743159cd
Ahsan <monjurul15-14516@diu.edu.bd>
2023-10-17 17:43:58 +0600

    update pg

--- src/components/admin-dashboard/Dashboard.jsx
+++ src/components/admin-dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Dashboard() {
-  return <div className="w-ful">Dashboard</div>;
+  return (
+    <><div className="w-ful">Dashboard</div>
+    <p>helooo</p></>
+  )
+  
 }
 
 export default Dashboard;
